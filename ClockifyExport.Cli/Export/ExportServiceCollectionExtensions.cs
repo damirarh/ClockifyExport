a30f86101a08ef77ba5c3d3a28e420b1c8480d64
Damir Arh <damir.arh@gmail.com>
2024-03-16 10:52:55 +0100

    Validate XML docs by generating documentation file
    Add missing documentation

--- ClockifyExport.Cli/Export/ExportServiceCollectionExtensions.cs
+++ ClockifyExport.Cli/Export/ExportServiceCollectionExtensions.cs
@@ -2,8 +2,16 @@
 
 namespace ClockifyExport.Cli.Export;
 
+/// <summary>
+/// Extension methods for <see cref="IServiceCollection"/> to add exporters.
+/// </summary>
 public static class ExportServiceCollectionExtensions
 {
+    /// <summary>
+    /// Adds all classes required by exporters to the service collection.
+    /// </summary>
+    /// <param name="services">The service collection to add the exporters to.</param>
+    /// <returns>The service collection with the exporters added.</returns>
     public static IServiceCollection AddExporters(this IServiceCollection services)
     {
         services.AddKeyedTransient<IExporter, CsvExporter>(ExportFormat.Csv);
