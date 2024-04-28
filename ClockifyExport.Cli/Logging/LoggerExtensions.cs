53311b284cfe8bd138d42e1ec2de4a39a92fea34
Damir Arh <damir.arh@gmail.com>
2024-04-28 09:10:32 +0200

    Use logging source generator for logigng methdos

--- ClockifyExport.Cli/Logging/LoggerExtensions.cs
+++ ClockifyExport.Cli/Logging/LoggerExtensions.cs
@@ -6,28 +6,25 @@ namespace ClockifyExport.Cli.Logging;
 /// <summary>
 /// Strongly typed logger extensions.
 /// </summary>
-public static class LoggerExtensions
+public static partial class LoggerExtensions
 {
-    private static readonly Action<
-        ILogger,
-        string,
-        ClockifyTimeEntry,
-        Exception?
-    > timeEntryValidationError = LoggerMessage.Define<string, ClockifyTimeEntry>(
-        LogLevel.Warning,
-        new EventId(1, nameof(TimeEntryValidationError)),
-        "Validation failed: {ValidationMessage}" + Environment.NewLine + "{TimeEntry}"
-    );
-
     /// <summary>
     /// Logs a time entry validation error.
     /// </summary>
     /// <param name="logger">Logger instance.</param>
     /// <param name="validationMessage">Validation error message to log.</param>
     /// <param name="timeEntry">Time entry that failed validation.</param>
-    public static void TimeEntryValidationError(
+    [LoggerMessage(
+        EventId = 1,
+        Level = LogLevel.Warning,
+        Message = """
+        Validation failed: {ValidationMessage}
+        {TimeEntry}
+        """
+    )]
+    public static partial void TimeEntryValidationError(
         this ILogger logger,
         string validationMessage,
         ClockifyTimeEntry timeEntry
-    ) => timeEntryValidationError(logger, validationMessage, timeEntry, default);
+    );
 }
